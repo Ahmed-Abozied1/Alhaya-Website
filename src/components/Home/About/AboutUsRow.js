26bcda7778339d204b65d5a45b6df8b36a576b04
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-03-27 00:08:02 +0200

    edit footer

--- src/components/Home/About/AboutUsRow.js
+++ src/components/Home/About/AboutUsRow.js
@@ -1,29 +1,22 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
 
-const AboutUsRow = ({title,para, imgSrc}) => {
+const AboutUsRow = ({ title, para, imgSrc }) => {
   return (
-<div className="  lg:grid lg:grid-cols-2 lg:gap-10 lg:mr-2 mr-6 items-center flex  lg:flex-col flex-col-reverse ">
-       
-       <div className="flex  lg:gap-4 gap-2 flex-col ">
+    <div className="  lg:grid lg:grid-cols-2 lg:gap-10 lg:mr-2 mr-6 items-center flex  lg:flex-col flex-col-reverse ">
+      <div className="flex  lg:gap-4 gap-2 flex-col ">
+        <h1 className="xl:text-3xl  text-2xl capitalize font-sans font-bold xl:leading-relaxed">
+          {title}
+        </h1>
+        <p className=" para   ">{para}</p>
+      </div>
+      <img
+        src={`/HomeImages/${imgSrc}`}
+        alt="Mobile app"
+        className="w-25 object-contain w-full  lg:w-auto flex justify-center"
+      />
+    </div>
+  );
+};
 
-         <h1 className="xl:text-3xl  text-xl capitalize font-sans font-medium xl:leading-relaxed">
-         {title}
-         </h1>
-         <p className=" para   ">
-        {para}
-         </p>
-         
-       </div>
-         <img
-        
-           src={`/HomeImages/${imgSrc}`}
-           alt="Mobile app"
-           className="w-25 object-contain w-full  lg:w-auto flex justify-center"
-         />
-       </div>
-   
-     )
-}
-
-export default AboutUsRow
+export default AboutUsRow;
