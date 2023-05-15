5bf3c2d18005213fbbd321e1de4f0a6051ebcae8
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-05-15 22:46:56 +0300

    edit About

--- src/components/Home/About/AboutUsRow.js
+++ src/components/Home/About/AboutUsRow.js
@@ -3,19 +3,19 @@ import "./About.css";
 
 const AboutUsRow = ({ title, para, imgSrc }) => {
   return (
-    <div className="  lg:grid lg:grid-cols-2 lg:gap-10 lg:mr-2 mr-0 items-center flex  lg:flex-col flex-col-reverse ">
-      <div className="flex  lg:gap-2 gap-2 flex-col ">
-        <h1 className="xl:text-3xl  text-2xl  aboutTitle   capitalize font-sans font-bold ">
-          {title}
-        </h1>
-        <p className=" para  aboutTitle ">{para}</p>
-      </div>
-      <img
-        src={`/HomeImages/${imgSrc}`}
-        alt="Mobile app"
-        className=" object-contain   lg:w-auto flex justify-center"
-      />
-    </div>
+    // <div className="  lg:grid lg:grid-cols-2 lg:gap-10 lg:mr-2 mr-0 items-center flex  lg:flex-col flex-col-reverse ">
+    //   <div className="flex  lg:gap-2 gap-2 flex-col ">
+    //     <h1 className="xl:text-3xl  text-2xl  aboutTitle   capitalize font-sans font-bold ">
+    //       {title}
+    //     </h1>
+    //     <p className=" para  aboutTitle ">{para}</p>
+    //   </div>
+    //   <img
+    //     src={`/HomeImages/${imgSrc}`}
+    //     alt="Mobile app"
+    //     className=" object-contain   lg:w-auto flex justify-center"
+    //   />
+    // </div>
   );
 };
 
