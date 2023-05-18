ee1531f9b7d3702fb20b45af49f5d2de688965b0
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-05-18 23:27:53 +0300

    services

--- src/components/Home/Services/ServicesRowChart.js
+++ src/components/Home/Services/ServicesRowChart.js
@@ -6,14 +6,14 @@ const ServicesRowChart = ({ img, title, title_ar }) => {
   const isRTL = i18n.language === "ar";
   return (
     <>
-      <div className="flex flex-col items-center">
+      {/* <div className="flex flex-col items-center">
         <div className="rounded-full lg:w-20 lg:h-20 w-16 h-16 bg-main lg:flex grid grid-flow-col justify-center  items-center">
           <img
             src={`/HomeImages/${img}`}
             alt=""
             className="lg:w-12 lg:h-12 w-10 h-10"
           />
-        </div>
+        </div> */}
         <p className="mt-4 text-white lg:text-xl ">
           {i18n.language === "en" ? title : title_ar}
         </p>
